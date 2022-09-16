5899355bd3652f0e3c532f06d7d96b7e239249ae
covidisahoax <OothecaPickle@users.noreply.github.com>
2022-09-15 20:03:09 -0700

    Update IntentPanel.java

--- desktop/src/com/limegroup/gnutella/gui/init/IntentPanel.java
+++ desktop/src/com/limegroup/gnutella/gui/init/IntentPanel.java
@@ -47,7 +47,7 @@ class IntentPanel extends JPanel {
         JLabel almostDone = new JLabel(I18n.tr("You're almost done!"));
         JLabel stateIntent = new JLabel(I18n.tr("State your intent below to start using FrostWire") + " " + FrostWireUtils.getFrostWireVersion());
         Line line = new Line();
-        MultiLineLabel description = new MultiLineLabel(I18n.tr("FrostWire is a peer-to-peer program for sharing unauthorized files only.  Installing and using the program explicitly constitutes a license for obtaining or distributing unauthorized content."), 500);
+        MultiLineLabel description = new MultiLineLabel(I18n.tr("FrostWire is a peer-to-peer program for sharing unauthorized files only.  Installing and using the program explicitly constitutes a license for obtaining and distributing unauthorized content."), 500);
         URLLabel findMore = new URLLabel("http://www.frostwire.com/?id=terms", I18n.tr("Find out more..."));
         Ditherer ditherer = new Ditherer(GUIUtils.hexToColor("E2E2E2"), GUIUtils.hexToColor("ECECEC"), Ditherer.Y_AXIS, new Ditherer.PolygonShader(2f));
         DitherPanel will = new DitherPanel(ditherer);
